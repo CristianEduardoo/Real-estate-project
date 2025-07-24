426f7401289801fb4a8479b5814e8e0027951892
CristianEduardoo <cj.94@hotmail.com>
2025-07-24 20:50:00 +0200

    feat: actualizar configuración de Gunicorn y eliminar configuración de Nginx

--- config/gunicorn/gunicorn_config.py
+++ config/gunicorn/gunicorn_config.py
@@ -1,5 +1,7 @@
-name = "BienesRaices"
-loglevel = 'info'
-errorlog = '-'
-accesslog ='-'
-workers = 2
+# Binding
+bind = "0.0.0.0:8000"  # Puerto interno del contenedor
+
+# Worker Processes
+workers = 3  # Número de procesos workers
+threads = 2  # Número de threads por worker
+timeout = 120  # Tiempo máximo para responder solicitudes
